35dcdfe5357628243a703c1409cd36609566450d
GOUDOUYAISSA <issagoudouya@gmail.com>
2022-04-27 10:51:00 +0000

    modifcation sur molec

--- src/Molec_m.f90
+++ src/Molec_m.f90
@@ -5,7 +5,7 @@ module Molec_m
 
   real(kind=Rk) :: mass = ONE
 
-  public :: Calc_pot,mass
+  public :: Calc_pot,mass,sub_pot
 
 contains
   FUNCTION Calc_pot(Q)
@@ -16,5 +16,20 @@ contains
   Calc_pot = mass*HALF * Q*Q
 
   END FUNCTION Calc_pot
+  ! for dibatic one need multidimenssional pot
+  SUBROUTINE  sub_pot(Mat_V,Q)
+    REAL(kind=Rk), intent(inout)   :: Mat_V(:,:,:)
+    REAL(kind=Rk), intent(in)      :: Q(:)
+    INTEGER                        :: iq
+
+    Do iq = 1, SIZE(Q)
+    Mat_V(Q(iq),1,1) = (Q(iq)+1)**2
+    Mat_V(Q(iq),2,2) = (Q(iq)-1)**2
+    Mat_V(Q(iq),1,2) = 0.01*Q(iq)
+    Mat_V(Q(iq),2,1) = 0.01*Q(iq)
+   END DO
+
+
+  END SUBROUTINE sub_pot
 
 end module Molec_m
